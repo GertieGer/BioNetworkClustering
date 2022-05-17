d3565e75f58f5f240244ba11b54945d38fd8c19f
talyasapir2 <talyasapir2@gmail.com>
2022-05-18 00:15:46 +0300

    testing

--- louvain+splitting/main.py
+++ louvain+splitting/main.py
@@ -75,11 +75,6 @@ for i in range(num_of_benchmarks):
             f = open(fpath, "a")
             ## NORMAL LOUVAIN
             print(network+"## NORMAL LOUVAIN")
-            start = time.time()
-            comms, dic = louvain.detect_communities(G)
-            end = time.time()
-            time_ = str(end - start)
-            write_result(f, network, "normal louvain", time_, comms, dic, real_comms)
             try:
                 start = time.time()
                 comms, dic = louvain.detect_communities(G)
