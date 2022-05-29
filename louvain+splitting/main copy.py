81c20f722a615e7a72c696ace38886bbae4b7d4f
talyasapir2 <talyasapir2@gmail.com>
2022-05-29 21:59:10 +0300

    newman

--- louvain+splitting/main copy.py
+++ louvain+splitting/main copy.py
@@ -77,99 +77,101 @@ for i in range(num_of_benchmarks):
             real_comms = get_comm_dic(commPath.format(network))
             G = nx.read_edgelist(netfile, nodetype=int)
 
-            f = open(fpath, "a")
-            ## NORMAL LOUVAIN
-            print(network+"## NORMAL LOUVAIN")
-            start = time.time()
-            comms, dic = louvain.detect_communities(G, randomized=True)
-            end = time.time()
-            time_ = str(end - start)
-            write_result(f, network, "normal louvain", time_, comms, dic, real_comms)
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, randomized=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "normal louvain", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",normal louvain,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");
-            f.close()
-
-            # ## NORMAL NEWMAN
-            # print(network+"## NORMAL NEWMAN")
+            # f = open(fpath, "a")
+            # ## NORMAL LOUVAIN
+            # print(network+"## NORMAL LOUVAIN")
+            # start = time.time()
+            # comms, dic = louvain.detect_communities(G, randomized=True)
+            # end = time.time()
+            # time_ = str(end - start)
+            # write_result(f, network, "normal louvain", time_, comms, dic, real_comms)
             # try:
             #     start = time.time()
-            #     dic = community_newman.partition(G)
-            #     comms = get_comms_from_dic(dic)
+            #     comms, dic = louvain.detect_communities(G, randomized=True)
             #     end = time.time()
             #     time_ = str(end - start)
-            #     write_result(f, network, "normal newman", time_, comms, dic, real_comms)
+            #     write_result(f, network, "normal louvain", time_, comms, dic, real_comms)
             # except:
             #     print("FAILED")
-            #     f.write(network+",normal newman,FAILED,FAILED,FAILED,FAILED")
+            #     f.write(network+",normal louvain,FAILED,FAILED,FAILED,FAILED")
             #     f.write("\n");
+            # f.close()
+
+            ## NORMAL NEWMAN
+            f = open(fpath, "a")
+            print(network+"## NORMAL NEWMAN")
+            try:
+                start = time.time()
+                dic = community_newman.partition(G)
+                comms = get_comms_from_dic(dic)
+                end = time.time()
+                time_ = str(end - start)
+                write_result(f, network, "normal newman", time_, comms, dic, real_comms)
+            except:
+                print("FAILED")
+                f.write(network+",normal newman,FAILED,FAILED,FAILED,FAILED")
+                f.write("\n");
+            f.close()
             
-            f = open(fpath, "a")
-            ## SPLIT LOUVAIN NO REMERERGE
-            print(network+"## SPLIT LOUVAIN NO REMERERGE")
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, splitting_func=louvain.louvainfunc, remerge=False, randomized=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "split_louvain", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",split_louvain,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");
-            f.close()
+            # f = open(fpath, "a")
+            # ## SPLIT LOUVAIN NO REMERERGE
+            # print(network+"## SPLIT LOUVAIN NO REMERERGE")
+            # try:
+            #     start = time.time()
+            #     comms, dic = louvain.detect_communities(G, splitting_func=louvain.louvainfunc, remerge=False, randomized=True)
+            #     end = time.time()
+            #     time_ = str(end - start)
+            #     write_result(f, network, "split_louvain", time_, comms, dic, real_comms)
+            # except:
+            #     print("FAILED")
+            #     f.write(network+",split_louvain,FAILED,FAILED,FAILED,FAILED")
+            #     f.write("\n");
+            # f.close()
 
-            f = open(fpath, "a")
-            ## SPLIT LOUVAIN WITH REMERERGE
-            print(network+"## SPLIT LOUVAIN WITH REMERERGE")
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, splitting_func=louvain.louvainfunc, remerge=True, randomized=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "split_louvain_remerge", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",split_louvain_remerge,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");
-            f.close()
+            # f = open(fpath, "a")
+            # ## SPLIT LOUVAIN WITH REMERERGE
+            # print(network+"## SPLIT LOUVAIN WITH REMERERGE")
+            # try:
+            #     start = time.time()
+            #     comms, dic = louvain.detect_communities(G, splitting_func=louvain.louvainfunc, remerge=True, randomized=True)
+            #     end = time.time()
+            #     time_ = str(end - start)
+            #     write_result(f, network, "split_louvain_remerge", time_, comms, dic, real_comms)
+            # except:
+            #     print("FAILED")
+            #     f.write(network+",split_louvain_remerge,FAILED,FAILED,FAILED,FAILED")
+            #     f.write("\n");
+            # f.close()
 
-            f = open(fpath, "a")
-            ## SPLIT GIRVAN NO REMERERGE
-            print(network+"## SPLIT GIRVAN NO REMERERGE")
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.newmanGirvan, remerge=False, randomized=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "split_girvan", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",split_girvan,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");
-            f.close()
+            # f = open(fpath, "a")
+            # ## SPLIT GIRVAN NO REMERERGE
+            # print(network+"## SPLIT GIRVAN NO REMERERGE")
+            # try:
+            #     start = time.time()
+            #     comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.newmanGirvan, remerge=False, randomized=True)
+            #     end = time.time()
+            #     time_ = str(end - start)
+            #     write_result(f, network, "split_girvan", time_, comms, dic, real_comms)
+            # except:
+            #     print("FAILED")
+            #     f.write(network+",split_girvan,FAILED,FAILED,FAILED,FAILED")
+            #     f.write("\n");
+            # f.close()
 
-            f = open(fpath, "a")
-            ## SPLIT GIRVAN WITH REMERERGE
-            print(network+"## SPLIT GIRVAN WITH REMERERGE")
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.newmanGirvan, remerge=True, randomized=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "split_girvan_remerge", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",split_girvan_remerge,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");             
-            f.close()
+            # f = open(fpath, "a")
+            # ## SPLIT GIRVAN WITH REMERERGE
+            # print(network+"## SPLIT GIRVAN WITH REMERERGE")
+            # try:
+            #     start = time.time()
+            #     comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.newmanGirvan, remerge=True, randomized=True)
+            #     end = time.time()
+            #     time_ = str(end - start)
+            #     write_result(f, network, "split_girvan_remerge", time_, comms, dic, real_comms)
+            # except:
+            #     print("FAILED")
+            #     f.write(network+",split_girvan_remerge,FAILED,FAILED,FAILED,FAILED")
+            #     f.write("\n");             
+            # f.close()
 
             # f = open(fpath, "a")
             # ## SPLIT NEWMAN NO REMERERGE
@@ -201,65 +203,65 @@ for i in range(num_of_benchmarks):
             #     f.write("\n");
             # f.close() 
 
-            f = open(fpath, "a")
-            ## SPLIT NX.GIRVAN_MOD NO REMERERGE
-            print(network+"## SPLIT NX.GIRVAN_MOD NO REMERERGE")     
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.networkxMaxModularity, remerge=False, randomized=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "split_nx.girvan_mod", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",split_nx.girvan_mod,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");
-            f.close() 
+            # f = open(fpath, "a")
+            # ## SPLIT NX.GIRVAN_MOD NO REMERERGE
+            # print(network+"## SPLIT NX.GIRVAN_MOD NO REMERERGE")     
+            # try:
+            #     start = time.time()
+            #     comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.networkxMaxModularity, remerge=False, randomized=True)
+            #     end = time.time()
+            #     time_ = str(end - start)
+            #     write_result(f, network, "split_nx.girvan_mod", time_, comms, dic, real_comms)
+            # except:
+            #     print("FAILED")
+            #     f.write(network+",split_nx.girvan_mod,FAILED,FAILED,FAILED,FAILED")
+            #     f.write("\n");
+            # f.close() 
 
-            ## SPLIT NX.GIRVAN_MOD WITH REMERERGE
-            f = open(fpath, "a")
-            print(network+"## SPLIT NX.GIRVAN_MOD WITH REMERERGE")
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.networkxMaxModularity, remerge=True, randomized=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "split_nx.girvan_mod_remerge", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",split_nx.girvan_mod_remerge,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");
-            f.close()
+            # ## SPLIT NX.GIRVAN_MOD WITH REMERERGE
+            # f = open(fpath, "a")
+            # print(network+"## SPLIT NX.GIRVAN_MOD WITH REMERERGE")
+            # try:
+            #     start = time.time()
+            #     comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.networkxMaxModularity, remerge=True, randomized=True)
+            #     end = time.time()
+            #     time_ = str(end - start)
+            #     write_result(f, network, "split_nx.girvan_mod_remerge", time_, comms, dic, real_comms)
+            # except:
+            #     print("FAILED")
+            #     f.write(network+",split_nx.girvan_mod_remerge,FAILED,FAILED,FAILED,FAILED")
+            #     f.write("\n");
+            # f.close()
 
-            ## SPLIT NX.GIRVAN_CON NO REMERERGE
-            f = open(fpath, "a")
-            print(network+"## SPLIT NX.GIRVAN_CON NO REMERERGE")
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.networkxMaxConductance, remerge=False, randomized=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "split_nx.girvan_con", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",split_nx.girvan_con,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");
-            f.close()
+            # ## SPLIT NX.GIRVAN_CON NO REMERERGE
+            # f = open(fpath, "a")
+            # print(network+"## SPLIT NX.GIRVAN_CON NO REMERERGE")
+            # try:
+            #     start = time.time()
+            #     comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.networkxMaxConductance, remerge=False, randomized=True)
+            #     end = time.time()
+            #     time_ = str(end - start)
+            #     write_result(f, network, "split_nx.girvan_con", time_, comms, dic, real_comms)
+            # except:
+            #     print("FAILED")
+            #     f.write(network+",split_nx.girvan_con,FAILED,FAILED,FAILED,FAILED")
+            #     f.write("\n");
+            # f.close()
             
-            ## SPLIT NX.GIRVAN_CON WITH REMERERGE
-            print(network+"## SPLIT NX.GIRVAN_CON WITH REMERERGE")
-            f = open(fpath, "a")
-            try:
-                start = time.time()
-                comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.networkxMaxConductance, remerge=True)
-                end = time.time()
-                time_ = str(end - start)
-                write_result(f, network, "split_nx.girvan_con_remerge", time_, comms, dic, real_comms)
-            except:
-                print("FAILED")
-                f.write(network+",split_nx.girvan_con_remerge,FAILED,FAILED,FAILED,FAILED")
-                f.write("\n");
-            f.close()
+            # ## SPLIT NX.GIRVAN_CON WITH REMERERGE
+            # print(network+"## SPLIT NX.GIRVAN_CON WITH REMERERGE")
+            # f = open(fpath, "a")
+            # try:
+            #     start = time.time()
+            #     comms, dic = louvain.detect_communities(G, splitting_func=newmanGirvan.networkxMaxConductance, remerge=True)
+            #     end = time.time()
+            #     time_ = str(end - start)
+            #     write_result(f, network, "split_nx.girvan_con_remerge", time_, comms, dic, real_comms)
+            # except:
+            #     print("FAILED")
+            #     f.write(network+",split_nx.girvan_con_remerge,FAILED,FAILED,FAILED,FAILED")
+            #     f.write("\n");
+            # f.close()
 
             f = open(fpath, "a")
             f.write("\n");
